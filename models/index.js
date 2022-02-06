156e868e573597a471b4f0e826698b0655a4d914
hy <yang.hrn@gmail.com>
2022-02-06 18:02:13 +0000

    models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,28 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  through: "ProductTag",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.hasMany(Product, {
+  through: "ProductTag",
+});
 
 module.exports = {
   Product,
